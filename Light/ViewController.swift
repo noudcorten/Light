e1d5ac4fb9dfd112570e78c4f4afaced73374f6a
Noud <mac@macs-macbook-pro.local>
2018-12-16 14:20:56 +0100

    last version

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,19 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textLabel: UILabel!
+    
     var lightOn = true
     
+    // MARK: if the screen is touched it changes the color and text
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn = !lightOn
         updateUI()
     }
+    
+    // MARK: when the view is loaded it changed the background
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
 
+    // MARK: updates the UI by changing the background color and the label's color and text
     func updateUI(){
         view.backgroundColor = lightOn ? .white : .black
+        if lightOn == true {
+            textLabel.textColor = .black
+            textLabel.text = "Turn off"
+        } else {
+            textLabel.textColor = .white
+            textLabel.text = "Turn on"
+        }
+        
     }
 }
 
